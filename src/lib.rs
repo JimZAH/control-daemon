f1a7325d247450a766a055fb4c22c579cfd20dbd
JimZAH <james@colderwood.com>
2023-05-11 19:38:34 +0100

    Load config from file

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use serde_derive::{Deserialize, Serialize};
+use std::io::Read;
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub host: String,
+    pub name: String,
+    pub topics: Vec<String>,
+}
+
+impl Config {
+    pub fn load() -> Config {
+        /* Unwrapping is fine here because if this fails then we can't run */
+        if let Ok(mut f) = std::fs::File::open("config.json") {
+            let mut buff = vec![];
+            f.read_to_end(&mut buff).unwrap();
+            let config = serde_json::from_slice::<Config>(&buff).unwrap();
+            return config;
+        }
+        Self {
+            host: "mqtt://10.145.0.4:1883".to_string(),
+            name: "GB3VW".to_string(),
+            topics: vec![
+                "repeater-control".to_string(),
+                "repeater-control/status".to_string(),
+                "repeater-control/lwt".to_string(),
+            ],
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
+use control::Config;
 use futures::{executor::block_on, stream::StreamExt};
 use paho_mqtt as mqtt;
 use serde_derive::{Deserialize, Serialize};
 use std::{
-    env,
     process::{self, Command},
     time::Duration,
 };
 
-const TOPICS: &[&str] = &[
-    "repeater-control",
-    "repeater-control/status",
-    "repeater-control/lwt",
-];
 const QOS: &[i32] = &[1];
 
 /*
@@ -21,63 +16,53 @@ const QOS: &[i32] = &[1];
 struct Cmd {
     command_type: String,
     command: String,
-}
-
-struct Config {
-    host: String,
-    name: String,
+    response_topic: String,
 }
 
 impl Cmd {
-    
     async fn exec(&self, args: Vec<&str>) -> Option<String> {
-        if let Ok(c) = Command::new("sh").arg("-c").args(args).output(){
+        if let Ok(c) = Command::new("sh").arg("-c").args(args).output() {
             /* unwrapping should be safe as we should never receive non utf8 from stdout */
-            return Some(std::str::from_utf8(&c.stdout).unwrap().to_string())
+            return Some(std::str::from_utf8(&c.stdout).unwrap().to_string());
         }
         None
-    } 
-    
-    async fn process(&self, c: &mqtt::AsyncClient) -> mqtt::Result<()> {
+    }
+
+    async fn process(&self, config: &Config, c: &mqtt::AsyncClient) -> mqtt::Result<()> {
         match self.command_type.chars().next() {
             Some('A') => {
                 /*
                  * Testing command
                  */
-                if let Some(s) = self.exec(vec!["asterisk", "-x", &self.command]).await{
-                    Cmd::response(c, &s).await?
+                if let Some(s) = self.exec(vec!["asterisk", "-x", &self.command]).await {
+                    Cmd::response(&config, c, &s).await?
                 }
             }
             Some('U') => {
-                if let Some(s) = self.exec(vec!["uptime"]).await{
-                    Cmd::response(c, &s).await?
+                if let Some(s) = self.exec(vec!["uptime"]).await {
+                    Cmd::response(&config, c, &s).await?
                 }
             }
             _ => {
                 println!("Command not valid");
-                Cmd::response(&c, "Command type is not valid").await?
+                Cmd::response(&config, &c, "Command type is not valid").await?
             }
         }
         Ok(())
     }
 
-    async fn response(c: &mqtt::AsyncClient, m: &str) -> mqtt::Result<()> {
-        let response = mqtt::message::Message::new(TOPICS[1], m, 1);
+    async fn response(config: &Config, c: &mqtt::AsyncClient, m: &str) -> mqtt::Result<()> {
+        let response = mqtt::message::Message::new(&config.topics[1], m, 1);
         c.publish(response).await
     }
 }
 
 fn main() {
-    let config = Config {
-        host: env::args()
-            .nth(1)
-            .unwrap_or_else(|| "mqtt://10.145.0.4:1883".to_string()),
-        name: "GB3VW".to_string(),
-    };
+    let config = Config::load();
 
     let create_opts = mqtt::CreateOptionsBuilder::new_v3()
-        .server_uri(config.host)
-        .client_id(config.name)
+        .server_uri(&config.host)
+        .client_id(&config.name)
         .finalize();
 
     let mut client = mqtt::AsyncClient::new(create_opts).unwrap_or_else(|e| {
@@ -89,7 +74,7 @@ fn main() {
         // Get message stream before connecting.
         let mut strm = client.get_stream(25);
 
-        let lwt = mqtt::Message::new(TOPICS[2], "ALIVE", mqtt::QOS_1);
+        let lwt = mqtt::Message::new(&config.topics[2], "ALIVE", mqtt::QOS_1);
 
         let conn_opts = mqtt::ConnectOptionsBuilder::new_v3()
             .keep_alive_interval(Duration::from_secs(30))
@@ -99,13 +84,13 @@ fn main() {
 
         client.connect(conn_opts).await?;
 
-        client.subscribe(TOPICS[0], QOS[0]).await?;
+        client.subscribe(&config.topics[0], QOS[0]).await?;
 
         while let Some(msg_opt) = strm.next().await {
             if let Some(msg) = msg_opt {
                 println!("{}", msg);
                 if let Ok(cmd) = serde_json::from_str::<Cmd>(&msg.payload_str()) {
-                    cmd.process(&client).await?
+                    cmd.process(&config, &client).await?
                 }
             } else {
                 println!("Lost connection. Attempting reconnect.");
